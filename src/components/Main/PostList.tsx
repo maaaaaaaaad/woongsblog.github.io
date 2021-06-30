a61e28aaaca72ebbf0fe9149ee67fdfb8055f075
DONG WOONG OH <integral5137@gmail.com>
2021-06-30 20:19:45 +0900

    Add to Media query design

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -18,10 +18,16 @@ const POST_ITEM_DATA = {
 const PostListWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-gap: 30px;
-  width: 1000px;
+  grid-gap: 20px;
+  width: 768px;
   margin: 0 auto;
   padding: 50px 0 100px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    width: 100%;
+    padding: 50px 20px;
+  }
 `;
 
 const PostList: FunctionComponent = function () {
--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -2,15 +2,22 @@ import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import myImage from '../../images/Typescript_logo.png';
 
+const myImg = myImage as string;
+
 const ProfileImageWrapper = styled.img`
   width: 120px;
   height: 120px;
   margin-bottom: 30px;
   border-radius: 50%;
+
+  @media (max-width: 768px) {
+    width: 80px;
+    height: 80px;
+  }
 `;
 
 const ProfileImage: FunctionComponent = function () {
-  return <ProfileImageWrapper src={myImage} alt="Profile Image" />;
+  return <ProfileImageWrapper src={myImg} alt="Profile Image" />;
 };
 
 export default ProfileImage;
